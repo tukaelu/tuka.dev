b0b28557a81b6371877d5bbb1c14e8eab0bc95d1
Tsukasa Nishiyama <nsymtks@gmail.com>
2023-02-15 22:08:36 +0900

    feat: setup TinaCMS

---
+++ .tina/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINACMS_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINACMS_TOKEN, // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "public/uploads/images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/contents",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
